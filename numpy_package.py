f5ead6ca6f3fa8ef9a4bc31bd4c34b3a531f2c16
Rick Mottola <mottola.rick@gmail.com>
2016-06-11 23:28:51 -0600

    numpy

---
+++ numpy_package.py
@@ -0,0 +1,42 @@
+# IMPORT NUMPY
+import numpy as np
+
+# NUMPY !!!!! -- NUMERIC PYTHON
+# NUMPY IS VERY USEFUL FOR ARRAYS AND DATA
+
+# RECAP OF LISTS
+# ______________
+# POWERFUL
+# COLLECTION OF VALUES
+# HOLD DIFFERENT DATA TYPES
+# CHANGE, ADD, REMOVE
+
+# HOWEVER, FOR DATA SCIENCE:
+# __________________________
+# NEED MATHEMATICAL OPERATIONS OVER COLLECTIONS
+# NEED SPEED!
+# THIS IS A PROBLEM WITH LISTS
+
+# NUMPY IS OUR SOLUTION
+# ______________________
+# CAN PERFORM CALCULATIONS OVER ENTIRE ARRAYS
+# EASY AND FAST
+# USE pip3 install numpy
+
+
+height = [1.73, 1.68, 1.71, 1.89, 1.79]
+weight = [65.4, 59.2, 63.6, 88.4, 68.7]
+
+# USE NUMPY TO CHANGE HEIGHT LIST INTO ARRAY
+np_height = np.array(height)
+print(np_height)
+
+# USE NUMPY TO CHANGE WEIGHT LIST INTO ARRAY
+np_weight = np.array(weight)
+print(np_weight)
+
+# NOW I CAN PERFORM MATHEMATICAL OPERATIONS TO THE ARRAY
+bmi = np_weight / np_height ** 2
+print(bmi)
+
+
